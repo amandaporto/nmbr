dce1b96b5647a2d0cc7027721268571fb1959e82
Amanda Porto <amandaporto@msn.com>
2015-08-26 15:03:28 -0400

    Lower guess

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,4 +1,10 @@
-random = Random.new
-random.rand(100)
+numb = rand(100)
 puts "What number am I thinking? Guess a number between 1 and 100."
-guess = gets.chomp
+guess = gets.chomp.to_f
+
+while true
+  if guess < numb
+    puts "Too low! Guess again."
+    guess = gets.chomp.to_f
+  end
+end
