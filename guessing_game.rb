3e6b432ea4ffaac5927c909a27afbed03a0e3116
Amanda Porto <amandaporto@msn.com>
2015-08-26 17:51:46 -0400

    Two methods included

--- guessing_game.rb
+++ guessing_game.rb
@@ -4,19 +4,25 @@ guess_array = Array.new
 
 def input(message, array)
   puts message
+  guess_left(array)
   input_guess = gets.chomp.to_f
   array << input_guess
 
   return input_guess
 end
 
+def guess_left(array)
+  left = array.length
+  puts "You are on guess #{left} of 5." unless left == 0
+end
+
 guess = input("What number am I thinking? Guess a number between 1 and 100.", guess_array)
 
 while guess_array.length < 5
   if guess < num
-    guess = input("Too low! Guess again.", guess_array)
+    guess = input("Too low!", guess_array)
   elsif guess > num
-    guess = input("Too high! Guess again.", guess_array)
+    guess = input("Too high!", guess_array)
   else guess == num
     puts "You got it, you mindreader!"
     exit
